b6c9aff6c0e9a48b73c9b0758d8cd45eb2da27ae
LaBen <153195738+DouLaBen@users.noreply.github.com>
2024-04-11 20:45:24 +0800

    Merge remote-tracking branch 'origin/main'

---
+++ 3.Kivy学习笔记/1.Kivy安装/1.1Kivy安装.py
@@ -0,0 +1,13 @@
+"""
+清华大学的镜像源地址下载安装(速度快）：
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests
+
+Kivy安装
+最简单的方法是安装 kivy-team 提供的 PyPi 轮子的当前稳定版本
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple "kivy[base]" kivy_examples
+
+要额外安装支持音频/视频的 Kivy，请安装Kivy 的依赖项。kivy[base,media]kivy[full]
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple "kivy[media]"
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple " kivy[full]"
+"""
